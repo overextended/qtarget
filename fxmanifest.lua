4b447818e10c6661a32521cf343bf6b4a454359a
Noms <nommyface@gmail.com>
2021-07-20 00:50:16 +0100

    Update fxmanifest.lua
    Fix polyzone dependency and imports.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,50 +3,24 @@ fx_version 'adamant'
 game 'gta5'
 
 dependencies {
-	"polyzone"
+	"PolyZone"
 }
 
 ui_page 'html/index.html'
 
 client_scripts {
 	'@es_extended/imports.lua',
-	'@polyzone/client.lua',
-	'@polyzone/BoxZone.lua',
-	'@polyzone/EntityZone.lua',
-	'@polyzone/CircleZone.lua',
-	'@polyzone/ComboZone.lua',
+	'@PolyZone/client.lua',
+	'@PolyZone/BoxZone.lua',
+	'@PolyZone/EntityZone.lua',
+	'@PolyZone/CircleZone.lua',
+	'@PolyZone/ComboZone.lua',
 	'config.lua',
-	'client/main.lua',
+	'client/main.lua'
 }
 
 files {
 	'html/index.html',
 	'html/css/style.css',
-	'html/js/script.js',
-	'html/css/all.min.css',
-	'html/webfonts/fa-brands-400.eot',
-	'html/webfonts/fa-brands-400.svg',
-	'html/webfonts/fa-brands-400.ttf',
-	'html/webfonts/fa-brands-400.woff',
-	'html/webfonts/fa-brands-400.woff2',
-	'html/webfonts/fa-duotone-900.eot',
-	'html/webfonts/fa-duotone-900.svg',
-	'html/webfonts/fa-duotone-900.ttf',
-	'html/webfonts/fa-duotone-900.woff',
-	'html/webfonts/fa-duotone-900.woff2',
-	'html/webfonts/fa-light-300.eot',
-	'html/webfonts/fa-light-300.svg',
-	'html/webfonts/fa-light-300.ttf',
-	'html/webfonts/fa-light-300.woff',
-	'html/webfonts/fa-light-300.woff2',
-	'html/webfonts/fa-regular-400.eot',
-	'html/webfonts/fa-regular-400.svg',
-	'html/webfonts/fa-regular-400.ttf',
-	'html/webfonts/fa-regular-400.woff',
-	'html/webfonts/fa-regular-400.woff2',
-	'html/webfonts/fa-solid-900.eot',
-	'html/webfonts/fa-solid-900.svg',
-	'html/webfonts/fa-solid-900.ttf',
-	'html/webfonts/fa-solid-900.woff',
-	'html/webfonts/fa-solid-900.woff2'
+	'html/js/script.js'
 }
