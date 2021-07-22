abc6409554f049610434eb543b0c3c34c37296d4
Linden <heliowaltonau@gmail.com>
2021-07-23 07:33:09 +1000

    refactor(config): Change spacing

--- config.lua
+++ config.lua
@@ -1,46 +1,12 @@
 Config = {}
 
-Config.LindenInventory = false -- Set to `true` if using linden_inventory.
+Config.VehicleBones = {'chassis', 'windscreen', 'seat_pside_r', 'seat_dside_r', 'bodyshell', 'suspension_lm', 'suspension_lr', 'platelight', 'attach_female', 'attach_male', 'bonnet', 'boot', 'chassis_dummy', 'chassis_Control', 'door_dside_f', 'door_dside_r', 'door_pside_f', 'door_pside_r', 'Gun_GripR', 'windscreen_f', 'platelight', 'VFX_Emitter', 'window_lf', 'window_lr', 'window_rf', 'window_rr', 'engine', 'gun_ammo', 'ROPE_ATTATCH', 'wheel_lf', 'wheel_lr', 'wheel_rf', 'wheel_rr', 'exhaust', 'overheat', 'misc_e', 'seat_dside_f', 'seat_pside_f', 'Gun_Nuzzle', 'seat_r'}
 
-Config.VehicleBones = {
-'chassis',  
-'windscreen',  
-'seat_pside_r',  
-'seat_dside_r',  
-'bodyshell',  
-'suspension_lm',  
-'suspension_lr',  
-'platelight',  
-'attach_female',  
-'attach_male',  
-'bonnet',  
-'boot',  
-'chassis_dummy',	
-'chassis_Control',	
-'door_dside_f',	
-'door_dside_r',	
-'door_pside_f',	
-'door_pside_r',	
-'Gun_GripR',  
-'windscreen_f',  
-'platelight',	
-'VFX_Emitter',  
-'window_lf',	
-'window_lr',	
-'window_rf',	
-'window_rr',	
-'engine',	
-'gun_ammo',  
-'ROPE_ATTATCH',	
-'wheel_lf',
-'wheel_lr',	
-'wheel_rf',	
-'wheel_rr',	
-'exhaust',	
-'overheat',	
-'misc_e',	
-'seat_dside_f',
-'seat_pside_f',
-'Gun_Nuzzle',  
-'seat_r',  
-}
+
+--------------------------------------------------------------------------------------------
+-- Settings
+--------------------------------------------------------------------------------------------
+-- Support when using linden_inventory
+Config.LindenInventory = true
+
+--------------------------------------------------------------------------------------------
