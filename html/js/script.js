4dfc86d2802819ebe1a2a0e4debecab3793a19bf
Linden <65407488+thelindat@users.noreply.github.com>
2021-11-10 13:22:59 +1100

    refactor(js): utilise Object.values and change formatting

--- html/js/script.js
+++ html/js/script.js
@@ -1,44 +1,54 @@
-window.addEventListener('message', function(event) {
-	let item = event.data;
+window.addEventListener("message", function (event) {
+  let item = event.data;
 
-	if (item.response == 'openTarget') {
-		$(".target-label").html("");
-		$('.target-wrapper').show();
-		$(".target-eye").css("color", "black");
+  if (item.response == "openTarget") {
+    $(".target-label").html("");
+    $(".target-wrapper").show();
+    $(".target-eye").css("color", "black");
+  } else if (item.response == "closeTarget") {
+    $(".target-label").html("");
+    $(".target-wrapper").hide();
+  } else if (item.response == "validTarget") {
+    $(".target-label").html("");
 
-	} else if (item.response == 'closeTarget') {
-		$(".target-label").html("");
-		$('.target-wrapper').hide();
+    Object.values(item.data).forEach((item, index) => {
+      $(".target-label").append(
+        "<div class='target-item' id='" +
+          index +
+          "'<li><i class='" +
+          item.icon +
+          " fa-fw fa-pull-left target-icon'></i>" +
+          item.label +
+          "</li></div>"
+      );
+      $("#target-" + index).hover((e) => {
+        $("#target-" + index).css(
+          "color",
+          e.type === "mouseenter" ? "rgb(98, 135, 236)" : "white"
+        );
+      });
+      $("#" + index).css("padding-top", "0.75vh");
+    });
 
-	} else if (item.response == 'validTarget') {
-		$(".target-label").html("");
-		$.each(item.data, function (index, item) {
-			$(".target-label").append("<div class='target-item' id='"+index+"'<li><i class='"+item.icon+" fa-fw fa-pull-left target-icon'></i>"+item.label+"</li></div>");
-			$("#target-"+index).hover((e)=> {
-				$("#target-"+index).css("color",e.type === "mouseenter"?"rgb(98, 135, 236)":"white")
-			})
-			$("#"+index).css("padding-top", "0.75vh");
-		});
-		$(".target-eye").css("color", "rgba(255, 255, 255, 0.8)");
-
-	} else if (item.response == 'leftTarget') {
-		$(".target-label").html("");
-		$(".target-eye").css("color", "black");
-	}
+    $(".target-eye").css("color", "rgba(255, 255, 255, 0.8)");
+  } else if (item.response == "leftTarget") {
+    $(".target-label").html("");
+    $(".target-eye").css("color", "black");
+  }
 });
 
-$(document).on('mousedown', (event) => {
-	let element = event.target;
-	$(".target-label").html("");
-	$('.target-wrapper').hide();
-	switch (event.which) {
-		case 1: {
-			$.post('https://qtarget/selectTarget', JSON.stringify(element.id + 1));
-			break;
-		}
-		case 3: {
-			$.post('https://qtarget/closeTarget');
-			break;
-		}
-	}
+$(document).on("mousedown", (event) => {
+  let element = event.target;
+  $(".target-label").html("");
+  $(".target-wrapper").hide();
+  switch (event.which) {
+    case 1: {
+      $.post("https://qtarget/selectTarget", JSON.stringify(element.id + 1));
+      break;
+    }
+    case 3: {
+      $.post("https://qtarget/closeTarget");
+      break;
+    }
+  }
 });
