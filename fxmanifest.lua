079834eebd31c3a51ae7dc2d4d876f090f6b43c5
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-04-19 12:18:25 +0200

    chore(manifest): bump version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ game         'gta5'
 --[[ Resource Information ]]--
 name         'qtarget'
 author       'Linden, Noms'
-version      '2.1.0'
+version      '2.1.1'
 repository   'https://github.com/overextended/qtarget'
 description  'An optimised targetting solution, based on bt-target'
 
