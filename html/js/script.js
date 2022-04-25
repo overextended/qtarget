1e346dabc23bac7cddb0a25b6c921d68af0b8a97
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-04-25 16:14:15 +0200

    refactor(html/script): document-wide event listeners
    - This also fixes the issue of the color of the text not changing with the selection

--- html/js/script.js
+++ html/js/script.js
@@ -11,17 +11,16 @@ window.addEventListener('message', function (event) {
   } else if (item.response == 'validTarget') {
     $('.target-label').html('');
 
-    for (let index in item.data) {
-      const itemData = item.data[index];
+    for (let [index, itemData] of Object.entries(item.data)) {
       const numberTest = Number(index);
+
       if (!isNaN(numberTest)) index = numberTest + 1;
+
       $('.target-label').append(
         `<div class='target-item' id='${index}'><i class='${itemData.icon} fa-fw fa-pull-left target-icon'></i>${itemData.label}</div>`
       );
-      $(`#target-${index}`).hover((e) => {
-        $(`#target-${index}`).css('color', e.type === 'mouseenter' ? 'rgb(98, 135, 236)' : 'white');
-      });
-      $('#' + index).css('padding-top', '0.75vh');
+
+      $(`#${index}`).css('padding-top', '0.75vh');
     }
 
     $('.target-eye').css('color', 'rgba(255, 255, 255, 0.8)');
@@ -35,13 +34,16 @@ $(document).on('mousedown', (event) => {
   switch (event.which) {
     case 1: {
       const id = event.target.id;
+
       if (id) $.post(`https://${GetParentResourceName()}/selectTarget`, JSON.stringify(id));
+
       $('.target-label').html('');
       $('.target-wrapper').hide();
       break;
     }
     case 3: {
       $.post(`https://${GetParentResourceName()}/leftTarget`);
+
       $('.target-label').html('');
       $('.target-eye').css('color', 'black');
       break;
@@ -54,9 +56,22 @@ $(document).on('keydown', (event) => {
     case 'Escape':
     case 'Backspace': {
       $.post(`https://${GetParentResourceName()}/closeTarget`);
+
       $('.target-label').html('');
       $('.target-wrapper').hide();
       $('.target-eye').css('color', 'black');
     }
   }
+});
+
+$(document).on('mouseover', (event) => {
+  const element = event.target;
+
+  if (element.id) $(`#${element.id}`).css('color', 'rgb(98, 135, 236)');
+});
+
+$(document).on('mouseout', (event) => {
+  const element = event.target;
+
+  if (element.id) $(`#${element.id}`).css('color', 'white');
 });
