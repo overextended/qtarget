ff589268e41e9774934ba20887f770ad8abc9eba
Haroki <rbtomas89@gmail.com>
2022-04-29 16:51:10 +0100

    feat(NUI): Recoded UI with AlpineJS
    - Will allow for easier further development for the UI
    - Thinking of integrating a sub-menu option

--- html/js/script.js
+++ html/js/script.js
@@ -11,17 +11,16 @@ window.addEventListener('message', function (event) {
   } else if (item.response == 'validTarget') {
     $('.target-label').html('');
 
-    for (let [index, itemData] of Object.entries(item.data)) {
-      if (itemData !== null) {
-        index = Number(index) + 1;
-
-        $('.target-label').append(
-          `<div class='target-item' id='${index}'><i class='${itemData.icon} fa-fw fa-pull-left target-icon'></i>${itemData.label}</div>`
-        );
-
-        $(`#${index}`).css('padding-top', '0.75vh');
-      }
-    }
+    Object.values(item.data).forEach((item, index) => {
+      index++;
+      $('.target-label').append(
+        `<div class='target-item' id='${index}'><i class='${item.icon} fa-fw fa-pull-left target-icon'></i>${item.label}</div>`
+      );
+      $(`#target-${index}`).hover((e) => {
+        $(`#target-${index}`).css('color', e.type === 'mouseenter' ? 'rgb(98, 135, 236)' : 'white');
+      });
+      $('#' + index).css('padding-top', '0.75vh');
+    });
 
     $('.target-eye').css('color', 'rgba(255, 255, 255, 0.8)');
   } else if (item.response == 'leftTarget') {
@@ -33,17 +32,14 @@ window.addEventListener('message', function (event) {
 $(document).on('mousedown', (event) => {
   switch (event.which) {
     case 1: {
-      const id = event.target.id;
-
-      if (id) $.post(`https://${GetParentResourceName()}/selectTarget`, JSON.stringify(id));
-
+      const id = event.target.id
+      if (id) $.post(`https://${GetParentResourceName()}/selectTarget`, JSON.stringify(Number(id)));
       $('.target-label').html('');
       $('.target-wrapper').hide();
       break;
     }
     case 3: {
       $.post(`https://${GetParentResourceName()}/leftTarget`);
-
       $('.target-label').html('');
       $('.target-eye').css('color', 'black');
       break;
@@ -56,22 +52,9 @@ $(document).on('keydown', (event) => {
     case 'Escape':
     case 'Backspace': {
       $.post(`https://${GetParentResourceName()}/closeTarget`);
-
       $('.target-label').html('');
       $('.target-wrapper').hide();
       $('.target-eye').css('color', 'black');
     }
   }
-});
-
-$(document).on('mouseover', (event) => {
-  const element = event.target;
-
-  if (element.id) $(`#${element.id}`).css('color', 'rgb(98, 135, 236)');
-});
-
-$(document).on('mouseout', (event) => {
-  const element = event.target;
-
-  if (element.id) $(`#${element.id}`).css('color', 'white');
 });
